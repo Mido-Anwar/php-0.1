7badfb33fd8630d55e71cad320ed3bffc6e4739a
Mido-Anwar <mimianwar33@hotmail.com>
2022-04-15 00:22:03 +0200

    folder strucure

---
+++ function/validate.php
@@ -0,0 +1,18 @@
+<?php
+
+function checkempty($value)
+{
+    if (empty($value)) {
+
+        return false;
+    }
+    return true;
+}
+function validateemail($email)
+{
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return false;
+    } else {
+        return true;
+    }
+}
